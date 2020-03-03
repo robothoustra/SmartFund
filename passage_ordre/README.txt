31e771121228fd30f913712a4e7b4bd6cc9748bd
robothoustra <alexis.robert1088@hotmail.fr>
2020-03-03 06:12:17 +0100

    modif README

--- passage_ordre/README.txt
+++ passage_ordre/README.txt
@@ -6,3 +6,24 @@
 	-Titres décimalisables
 	-Arrondi sur les quantités
 - Gestion de la couverture
+
+Colonnes Portefeuille réel (ancien protefeuille) :
+	TICKER
+	TYPE (ACTIONS, OBLIG ...)
+	QUANTITE	
+
+Colonnes Portefeuille théorique :
+	TICKER
+	POIDS
+	PRIX (nouveaux)
+	
+Doit fournir un fichier de contrainte : 
+	col 1 = ticker
+	col 2 = prix (pour une quantité)
+	col 3 = décimalisable (booléen)
+	col 4 = Min Qte
+
+Montant à répartir entre les actifs.
+
+Appliquer la méthode des moindres carré pour chaque catégorie d'actifs en fonction de la somme qui leur
+ait attribuée.
--- passage_ordre/passage_ordre/passage_ordre.py
+++ passage_ordre/passage_ordre/passage_ordre.py
@@ -6,6 +6,7 @@ import os
 # https://docs.scipy.org/doc/scipy/reference/optimize.html
 # https://docs.scipy.org/doc/scipy/reference/tutorial/optimize.html
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.least_squares.html
+
 class readable_dir(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
         prospective_dir=values
@@ -23,7 +24,7 @@ def load_arguments(**kwargs):
     parser.add_argument("-pt","--ptf_th", help="Chemin vers le fichier contenant le portefeuille théorique",
                         type=str, default='./output/ptfs.csv')
     parser.add_argument("-ic","--inst_const", help="Chemin vers le fichier contenant la liste des instruments avec leur contraintes en colonne",
-                        type=str, default='./output/.csv')
+                        type=str, default='./output/contraintes.csv')
     parser.add_argument("-dt","--date", help="Date du portefeuille (utile pour récupérer les prtfs dans les fichiers)",
                         type=str, default='./output/ptfs.csv')
 

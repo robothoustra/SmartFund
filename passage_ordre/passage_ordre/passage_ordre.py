7966d7697cb356c401e67f2fb3de87974595c7de
Alexis ROBERT <alexis.robert1088@hotmail.fr>
2020-03-05 16:44:36 +0100

    Modif passage ordre

--- passage_ordre/passage_ordre/passage_ordre.py
+++ passage_ordre/passage_ordre/passage_ordre.py
@@ -21,9 +21,9 @@ class readable_dir(argparse.Action):
 def load_arguments(**kwargs):
     parser = argparse.ArgumentParser()
     parser.add_argument("-pr","--ptf_reel", help="Chemin vers le fichier contenant le dernier portefeuille",
-                        type=str, default='../../selection/selection/data/ptfs réels.csv')
+                        type=str, default='../../selection/selection/output/prtfs_reels.csv')
     parser.add_argument("-pt","--ptf_th", help="Chemin vers le fichier contenant le portefeuille théorique",
-                        type=str, default='../../selection/selection/output/ptfs.csv')
+                        type=str, default='../../selection/selection/output/prtfs.csv')
     #parser.add_argument("-ic","--inst_const", help="Chemin vers le fichier contenant la liste des instruments avec leur contraintes en colonne",
                         #type=str, default='./output/contraintes.csv')
     parser.add_argument("-dtn","--date_new", help="Date du portefeuille cible",
@@ -48,16 +48,20 @@ def GetOrders(**kwargs):
     dtPTF_old = datetime.strptime(args.date_old,"%d/%m/%Y")
     
     #Liste des colonnes à récupérer dans le fichier du dernier portefeuille
-    list_col = ['TICKER','POIDS','COURS']
+    list_col_th = ['TICKER','POIDS','COURS']
+    list_col_reel = ['TCIKER','QTE']
     
-    #Lecture des données
+    #Lecture des données        
     dfPtfReel = pd.read_csv(args.ptf_reel,header=[0], sep=';', parse_dates=['DATE_PRTF'])
-    dfPtfCible = pd.read_csv(args.ptf_th, header=0, sep=';', parse_dates=['DATE_PRTF'])
+    dfPtfCible = pd.read_csv(args.ptf_th, header=[0], sep=';', parse_dates=['DATE_PRTF'])
     
     dfPtfReel = dfPtfReel[(dfPtfReel['DATE_PRTF'] == dtPTF_old)]
-    dfPtfCible = dfPtfReel[(dfPtfReel['DATE_PRTF'] == dtPTF_cible)]
+    dfPtfReel = dfPtfReel[list_col_reel]
     
-    print(dfPtfReel)
+    dfPtfCible = dfPtfCible[(dfPtfCible['DATE_PRTF'] == dtPTF_cible)]
+    dfPtfCible = dfPtfCible[list_col_th]
+    
+    print(dfPtfCible)
     
 if __name__ == '__main__':
     GetOrders()
